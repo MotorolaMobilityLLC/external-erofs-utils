erofs/erofs-utils

commit 1f11cf0463b503e86af3e07a84feafe05c0b10d7

erofs-utils: introduce extented options setting
Introduce option "-E" for extented options.
Legacy images (linux-4.19~5.2) can be generated by "-E legacy-compress".

Signed-off-by: Gao Xiang <gaoxiang25@huawei.com>

mkfs/main.c
@@ -27,6 +27,7 @@ static void usage(void)
 	fprintf(stderr, "Generate erofs image from DIRECTORY to FILE, and [options] are:\n");
 	fprintf(stderr, " -zX[,Y]   X=compressor (Y=compression level, optional)\n");
 	fprintf(stderr, " -d#       set output message level to # (maximum 9)\n");
+	fprintf(stderr, " -EX[,...] X=extended options\n");
 }
 
 u64 parse_num_from_str(const char *str)
@@ -39,11 +40,55 @@ u64 parse_num_from_str(const char *str)
 	return num;
 }
 
+static int parse_extended_opts(const char *opts)
+{
+#define MATCH_EXTENTED_OPT(opt, token, keylen) \
+	(keylen == sizeof(opt) && !memcmp(token, opt, sizeof(opt)))
+
+	const char *token, *next, *tokenend, *value __maybe_unused;
+	unsigned int keylen, vallen;
+
+	value = NULL;
+	for (token = opts; *token != '\0'; token = next) {
+		const char *p = strchr(token, ',');
+
+		next = NULL;
+		if (p)
+			next = p + 1;
+		else {
+			p = token + strlen(token);
+			next = p;
+		}
+
+		tokenend = memchr(token, '=', p - token);
+		if (tokenend) {
+			keylen = tokenend - token;
+			vallen = p - tokenend - 1;
+			if (!vallen)
+				return -EINVAL;
+
+			value = tokenend + 1;
+		} else {
+			keylen = p - token;
+			vallen = 0;
+		}
+
+		if (MATCH_EXTENTED_OPT("legacy-compress", token, keylen)) {
+			if (vallen)
+				return -EINVAL;
+			/* disable compacted indexes and 0padding */
+			cfg.c_legacy_compress = true;
+			sbi.requirements &= ~EROFS_REQUIREMENT_LZ4_0PADDING;
+		}
+	}
+	return 0;
+}
+
 static int mkfs_parse_options_cfg(int argc, char *argv[])
 {
 	int opt, i;
 
-	while ((opt = getopt(argc, argv, "d:z:")) != -1) {
+	while ((opt = getopt(argc, argv, "d:z:E:")) != -1) {
 		switch (opt) {
 		case 'z':
 			if (!optarg) {
@@ -64,6 +109,12 @@ static int mkfs_parse_options_cfg(int argc, char *argv[])
 
 		case 'd':
 			cfg.c_dbg_lvl = parse_num_from_str(optarg);
+			break;
+
+		case 'E':
+			opt = parse_extended_opts(optarg);
+			if (opt)
+				return opt;
 			break;
 
 		default: /* '?' */

README
@@ -57,6 +57,17 @@ Currently lz4 and lz4hc are available for compression, e.g.
 Or leave all files uncompressed as a option:
  $ mkfs.erofs foo.erofs.img foo/
 
+How to generate legacy erofs images
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Decompression inplace and compacted indexes have been introduced in
+linux-5.3, which are not backward-compatible with older kernels.
+
+In order to generate _legacy_ erofs images for old kernels,
+add "-E legacy-compress" to the command line, e.g.
+
+ $ mkfs.erofs -E legacy-compress -zlz4hc foo.erofs.img foo/
+
 Obsoleted erofs.mkfs
 ~~~~~~~~~~~~~~~~~~~~
 

include/erofs/defs.h
@@ -155,5 +155,9 @@ typedef int64_t         s64;
 #define DBG_BUGON(condition)	BUG_ON(condition)
 #endif
 
+#ifndef __maybe_unused
+#define __maybe_unused      __attribute__((__unused__))
+#endif
+
 #endif
 

MotorolaMobilityLLC/external-erofs-utils

commit 1e8e00329b12d551e6054fac0033da69a8ad470c

erofs-utils: fix up "-E legacy-compress"
"-E legacy-compress" isn't parsed properly, fix it now.

Reported-by: Li Guifu <blucerlee@gmail.com>
Fixes: 1f11cf0463b5 ("erofs-utils: introduce extented options setting")
Reviewed-by: Li Guifu <blucerlee@gmail.com>
Signed-off-by: Gao Xiang <hsiangkao@aol.com>

mkfs/main.c
@@ -34,7 +34,7 @@ static void usage(void)
 static int parse_extended_opts(const char *opts)
 {
 #define MATCH_EXTENTED_OPT(opt, token, keylen) \
-	(keylen == sizeof(opt) && !memcmp(token, opt, sizeof(opt)))
+	(keylen == sizeof(opt) - 1 && !memcmp(token, opt, sizeof(opt) - 1))
 
 	const char *token, *next, *tokenend, *value __maybe_unused;
 	unsigned int keylen, vallen;